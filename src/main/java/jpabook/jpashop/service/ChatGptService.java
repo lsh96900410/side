db862540777c635533c8cf3993370f0b4d9cee89
승현 <lsh96900410@gamil.com>
2024-05-16 23:28:15 +0900

    okhttp 관련 코드 제거

--- src/main/java/jpabook/jpashop/service/ChatGptService.java
+++ src/main/java/jpabook/jpashop/service/ChatGptService.java
@@ -1,12 +1,9 @@
 package jpabook.jpashop.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.OkHttpClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatGptService {
 
-    private final OkHttpClient httpClient = new OkHttpClient();
-    private final ObjectMapper objectMapper = new ObjectMapper();
 }
